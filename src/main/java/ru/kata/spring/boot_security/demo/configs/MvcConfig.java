63b96e04d20976030786635cc9b0af518fa4c4f8
Ildar Khuzin <ildar.khuzin75@mail.ru>
2024-11-06 16:50:16 +0300

    проба2

--- src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
@@ -3,6 +3,7 @@ package ru.kata.spring.boot_security.demo.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +12,7 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
     }
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
